b0a0525ad70bea27ac3144604302b06298986f1e
Alexandre Carli <carli.alex.13@gmail.com>
2025-07-24 16:56:06 +0200

    observateur

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
 
-  <input type="text" v-model="name">
+  <input type="text" v-model="page.title">
+  Temps ecoulé : {{  time }}
+
+  <button @click="reset">Reset</button>
 
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch, watchEffect } from 'vue';
+import {useTimer} from './composable/userTimer.js';
 
-const name = ref('');
+const page = ref({
+  title: ''
+});
 
-watch(name, (newValue, oldValue) => {
-  document.title = newValue
-})
+// watch( () => page.value.title, (newValue, oldValue) => {
+//   document.title = newValue
+// }, { immediate: true });
+
+
+watchEffect(() => {
+  document.title = page.value.title;
+});
+
+const { time, reset } = useTimer()
 </script>
